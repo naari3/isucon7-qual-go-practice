3ccb3a0e4d03ebf76ace8debdc1b9bdc9dd9918f
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-09-03 02:06:48 +0900

    tmp maki

--- src/isubata/app.go
+++ src/isubata/app.go
@@ -355,8 +355,14 @@ func postMessage(c echo.Context) error {
 	return c.NoContent(204)
 }
 
-func jsonifyMessage(m Message, u User) (map[string]interface{}, error) {
-	messageUser := User{Name: u.Name, DisplayName: u.DisplayName, AvatarIcon: u.AvatarIcon}
+func jsonifyMessage(m Message, _ User) (map[string]interface{}, error) {
+	// messageUser := User{Name: u.Name, DisplayName: u.DisplayName, AvatarIcon: u.AvatarIcon}
+	u := User{}
+	err := db.Get(&u, "SELECT name, display_name, avatar_icon FROM user WHERE id = ?",
+		m.UserID)
+	if err != nil {
+		return nil, err
+	}
 
 	r := make(map[string]interface{})
 	r["id"] = m.ID
