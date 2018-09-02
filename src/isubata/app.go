68347a1327510c717ae45f0f8675a9f8f5dc47cd
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-09-03 02:25:39 +0900

    aa

--- src/isubata/app.go
+++ src/isubata/app.go
@@ -366,7 +366,7 @@ func jsonifyMessage(m Message, u User) (map[string]interface{}, error) {
 
 	r := make(map[string]interface{})
 	r["id"] = m.ID
-	r["user"] = u
+	r["user"] = messageUser
 	r["date"] = m.CreatedAt.Format("2006/01/02 15:04:05")
 	r["content"] = m.Content
 	return r, nil
